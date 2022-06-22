4e4f40cb85b135b672dec8ec26ff27b4425df6c8
Murat Denizli <muratdenizli0@gmail.com>
2022-06-22 14:49:06 +0300

    change box component

--- src/Components/Box/index.js
+++ src/Components/Box/index.js
@@ -9,7 +9,7 @@ export default function Box({ title, id }) {
             <Link to={`/detail/${id}/${title}`}>
                 <div className='box-title'>
                     <img src='https://img-altinbasstore.mncdn.com/resim/pirlanta/INS309-8K.JPG' className='urun-resmi' alt='ürün' />
-                    <h2>Ürün Adı:{title}</h2>
+                    <h2>Ürün Adı: {title}</h2>
                 </div>
             </Link>
         </div>
